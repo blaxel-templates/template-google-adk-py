a4515ed0d7319a6f1cb407db92bf872017a0185d
cploujoux <cploujoux@blaxel.ai>
2025-06-19 11:45:00 -0700

    chore: update version

--- src/agent.py
+++ src/agent.py
@@ -1,9 +1,7 @@
-import os
 from logging import getLogger
 from typing import AsyncGenerator
 
-from blaxel.models import bl_model
-from blaxel.tools import bl_tools
+from blaxel.googleadk import bl_model, bl_tools
 from google.adk.agents import Agent
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
@@ -11,6 +9,7 @@ from google.genai import types
 
 logger = getLogger(__name__)
 session_service = InMemorySessionService()
+
 
 # @title Define the get_weather Tool
 def weather(city: str) -> str:
@@ -24,28 +23,40 @@ def weather(city: str) -> str:
     """
     return f"The weather in {city} is sunny"
 
+
 APP_NAME = "research_assistant"
 
+
 async def agent(input: str, user_id: str, session_id: str) -> AsyncGenerator[str, None]:
-    description = "You are a helpful assistant that can answer questions and help with tasks."
+    description = (
+        "You are a helpful assistant that can answer questions and help with tasks."
+    )
     prompt = """
 You are a helpful assistant that can answer questions about weather, places and more generic questions about real time information.
 """
-    tools = await bl_tools(["blaxel-search"], timeout_enabled=False).to_google_adk() + [weather]
-    model = await bl_model("sandbox-openai").to_google_adk()
+    tools = await bl_tools(["blaxel-search"], timeout_enabled=False) + [weather]
+    model = await bl_model("sandbox-openai")
 
-    agent = Agent(model=model, name=APP_NAME, description=description, instruction=prompt, tools=tools)
+    agent = Agent(
+        model=model,
+        name=APP_NAME,
+        description=description,
+        instruction=prompt,
+        tools=tools,
+    )
 
     # Create the specific session where the conversation will happen
-    session = await session_service.get_session(app_name=APP_NAME, user_id=user_id, session_id=session_id)
+    session = await session_service.get_session(
+        app_name=APP_NAME, user_id=user_id, session_id=session_id
+    )
     if not session:
         session = await session_service.create_session(
-            app_name=APP_NAME,
-            user_id=user_id,
-            session_id=session_id
+            app_name=APP_NAME, user_id=user_id, session_id=session_id
         )
 
-    logger.info(f"Session created: App='{APP_NAME}', User='{user_id}', Session='{session_id}'")
+    logger.info(
+        f"Session created: App='{APP_NAME}', User='{user_id}', Session='{session_id}'"
+    )
 
     runner = Runner(
         agent=agent,
@@ -54,11 +65,15 @@ You are a helpful assistant that can answer questions about weather, places and 
     )
     logger.info(f"Runner created for agent '{runner.agent.name}'.")
     content = types.Content(role="user", parts=[types.Part(text=input)])
-    async for event in runner.run_async(user_id=user_id, session_id=session_id, new_message=content):
-      # Key Concept: is_final_response() marks the concluding message for the turn.
-      if event.is_final_response():
-        if event.content and event.content.parts:
-            # Assuming text response in the first part
-            yield event.content.parts[0].text
-        elif event.actions and event.actions.escalate: # Handle potential errors/escalations
-            yield f"Agent escalated: {event.error_message or 'No specific message.'}"
+    async for event in runner.run_async(
+        user_id=user_id, session_id=session_id, new_message=content
+    ):
+        # Key Concept: is_final_response() marks the concluding message for the turn.
+        if event.is_final_response():
+            if event.content and event.content.parts:
+                # Assuming text response in the first part
+                yield event.content.parts[0].text
+            elif (
+                event.actions and event.actions.escalate
+            ):  # Handle potential errors/escalations
+                yield f"Agent escalated: {event.error_message or 'No specific message.'}"
--- src/server/router.py
+++ src/server/router.py
@@ -1,6 +1,6 @@
 import uuid
 
-from blaxel.instrumentation.span import SpanManager
+from blaxel.telemetry.span import SpanManager
 from fastapi import APIRouter, Request
 from fastapi.responses import StreamingResponse
 
@@ -8,13 +8,15 @@ from ..agent import agent
 
 router = APIRouter()
 
+
 @router.post("/")
 async def handle_request(request: Request):
     user_id = request.headers.get("X-User-Id", str(uuid.uuid4()))
     session_id = request.headers.get("X-Session-Id", str(uuid.uuid4()))
     body = await request.json()
-    with SpanManager("blaxel-google-adk").create_active_span("agent-request", {"user_id": user_id, "session_id": session_id}):
+    with SpanManager("blaxel-google-adk").create_active_span(
+        "agent-request", {"user_id": user_id, "session_id": session_id}
+    ):
         return StreamingResponse(
-            agent(body["inputs"], user_id, session_id),
-            media_type='text/event-stream'
+            agent(body["inputs"], user_id, session_id), media_type="text/event-stream"
         )
